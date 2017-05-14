587bf8285afbf7176deb1799d7a62f11f5ea7adb
twumasid <twumasid@users.noreply.github.com>
2017-05-14 18:46:12 +0200

    UntilWordCell

---
+++ HomeController.swift
@@ -0,0 +1,48 @@
+//
+//  HomeController.swift
+//  yT-Twitter
+//
+//  Created by daniel on 14.05.17.
+//  Copyright © 2017 daniel. All rights reserved.
+//
+
+import UIKit
+
+class WordCell: UICollectionViewCell{
+    //this gets called when a cell is dequeued
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .yellow
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+}
+
+class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+   
+    let cellId = "cellId"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView?.backgroundColor = .green
+        collectionView?.register(WordCell.self, forCellWithReuseIdentifier: cellId)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 50)
+    }
+    
+    
+}
